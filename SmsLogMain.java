6b9d8b36fd2d55832acc09e8b33dd479fe8a9266
DCJoney <1050617313@qq.com>
2018-11-27 17:02:23 +0800

    first commit2

--- SmsLogMain.java
+++ SmsLogMain.java
@@ -5,36 +5,12 @@ import com.nxt.mms.logger.CommonLogger;
 import java.io.UnsupportedEncodingException;
 
 public class SmsLogMain {
-    void init() throws UnsupportedEncodingException {
-//       Logger logger = Logger.getLogger("@@@");
-//       logger.setParent();
-//       LogManager.getLogManager().addLogger(logger);
-//       ConsoleHandler handler = new ConsoleHandler();
-//       handler.setLevel(Level.INFO);
-//       handler.setEncoding("GBK");
-//       handler.setFormatter(new Formatter() {
-//           @Override
-//           public String format(LogRecord record) {
-//               return "";
-//           }
-//       });
-//        System.out.println(handler.getFormatter());
-//       logger.addHandler(handler);
-//        logger.info("1111");
-
+    public static void main(String[] args) throws UnsupportedEncodingException {
         CommonLogger<SmsLogMain> log = new CommonLogger<>(SmsLogMain.class);
-//        log.debug(null);
+        log.debug("2222");
         log.info("111111");
         log.warning("111111");
         log.error("111111");
-//        new Thread(()-> defaultLogger.info("111")).start();
     }
-
-
-    public static void main(String[] args) throws UnsupportedEncodingException {
-        new SmsLogMain().init();
-    }
-
-
 
 }
